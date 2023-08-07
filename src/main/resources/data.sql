baabc13f30bb90f47d75b6b435ab087b4d3727ac
ironmask431 <ironmask431@gmail.com>
2023-08-07 23:13:05 +0900

    6장 - blog API만들기 - domain 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into member (id, name) values (1,'송하영');
-insert into member (id, name) values (2,'박지원');
-insert into member (id, name) values (3,'이나경');
+--insert into member (id, name) values (1,'송하영');
+--insert into member (id, name) values (2,'박지원');
+--insert into member (id, name) values (3,'이나경');
