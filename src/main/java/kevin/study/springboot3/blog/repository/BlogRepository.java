d7615bd47bd844d94960ee5b20bc6cd5b03f7ef6
ironmask431 <ironmask431@gmail.com>
2023-08-08 22:44:31 +0900

    6장 - blog API만들기 - controller, service

--- src/main/java/kevin/study/springboot3/blog/repository/BlogRepository.java
+++ src/main/java/kevin/study/springboot3/blog/repository/BlogRepository.java
@@ -3,5 +3,5 @@ package kevin.study.springboot3.blog.repository;
 import kevin.study.springboot3.blog.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface BlogRepository extends JpaRepository<Article, Long> {
 }
---
+++ src/main/java/kevin/study/springboot3/blog/service/BlogService.java
@@ -0,0 +1,21 @@
+package kevin.study.springboot3.blog.service;
+
+
+import kevin.study.springboot3.blog.domain.Article;
+import kevin.study.springboot3.blog.dto.AddArticleRequest;
+import kevin.study.springboot3.blog.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
+
+
+
+}
